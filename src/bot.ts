58cefb5d68d006eccbb6ffb9c1c0b46633fb028d
ianagbip1oti <ianagbip1oti@gmail.com>
2021-01-10 21:10:21 +0000

    Remove inhibitor

--- src/bot.ts
+++ src/bot.ts
@@ -4,12 +4,7 @@ import "dotenv/config";
 import BotClient from "./client/client";
 
 const client: BotClient = new BotClient({
-  token: process.env.TOKEN,
-  channels: new Collection([
-    ["bot-commands", "353544874034855936"],
-    ["discord-js", "713041505719287818"],
-    //TODO add channels here, will be converted into an array, only using Map to add visualization on channel names
-  ]),
+  token: process.env.TOKEN
 });
 
 client.on("error", (error) => console.error(error));
--- src/client/client.ts
+++ src/client/client.ts
@@ -1,16 +1,13 @@
 import { AkairoClient, CommandHandler, InhibitorHandler, ListenerHandler } from "discord-akairo";
-import { Collection } from "discord.js";
 import { join } from "path";
 
 interface BotConfig {
   token: string;
-  channels: Collection<string, string>;
 }
 
 export default class BotClient extends AkairoClient {
   public commandHandler: CommandHandler;
   public listenerHandler: ListenerHandler;
-  public inhibitorHandler: InhibitorHandler;
   public config: BotConfig;
 
   constructor(config: BotConfig) {
@@ -50,29 +47,19 @@ export default class BotClient extends AkairoClient {
     this.listenerHandler = new ListenerHandler(this, {
       directory: join(__dirname, "..", "events"),
     });
-
-    /**
-     * Creates inhibitor handler
-     */
-    this.inhibitorHandler = new InhibitorHandler(this, {
-      directory: join(__dirname, "..", "inhibitors"),
-    });
   }
 
   private async initializeBot() {
     //Attach handlers
     this.listenerHandler.setEmitters({
-      InhibitorHandler: this.inhibitorHandler,
       commandHandler: this.commandHandler,
       listenerHandler: this.listenerHandler,
     });
 
     //Load handlers
     this.commandHandler.useListenerHandler(this.listenerHandler);
-    this.commandHandler.useInhibitorHandler(this.inhibitorHandler);
     this.listenerHandler.loadAll();
     this.commandHandler.loadAll();
-    this.inhibitorHandler.loadAll();
   }
 
   public async start(): Promise<string> {
