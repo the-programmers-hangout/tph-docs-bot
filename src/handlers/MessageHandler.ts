96f190cca42da2f2a5884817b0f41b001fbb7b64
ben <88249114+BenjammingKirby@users.noreply.github.com>
2021-12-20 11:34:08 -0700

    Add status command

--- src/handlers/MessageHandler.ts
+++ src/handlers/MessageHandler.ts
@@ -1,45 +1,44 @@
-import { intervalToDuration } from "date-fns";
 import { Formatters, MessageEmbed } from "discord.js";
 import type { Message } from "discord.js";
+import { intervalToDuration, intervalObjToStr } from "../utils/DateUtils";
 
 export async function messageHandler(message: Message<true>) {
-    const clientUser = message.client.user;
-    // The regex for the bot's mention
-    const mentionRegex = new RegExp(`<@!?${clientUser.id}>`);
+    try {
+        const clientUser = message.client.user;
+        // The regex for the bot's mention
+        const mentionRegex = new RegExp(`<@!?${clientUser.id}>`);
 
-    if (message.content.trim().match(mentionRegex)) {
-        const pkgJSONPath = "../../package.json";
-        const pkgJSON = await import(pkgJSONPath);
-        const { version, description, dependencies } = pkgJSON;
+        if (message.content.trim().match(mentionRegex)) {
+            const pkgJSONPath = "../../package.json";
+            const pkgJSON = await import(pkgJSONPath);
+            const { version, description, dependencies } = pkgJSON;
 
-        const uptime = intervalToDuration({
-            start: new Date().getTime() - message.client.uptime,
-            end: new Date().getTime(),
-        });
+            const uptime = intervalToDuration(Date.now() - message.client.uptime, Date.now());
+            const statusEmbed = new MessageEmbed()
+                .setTitle(`${clientUser.username} (v${version})`)
+                .setURL("https://github.com/the-programmers-hangout/tph-docs-bot/")
+                .setColor(0xd250c7)
+                .setDescription(description)
+                .setThumbnail(clientUser.displayAvatarURL({ dynamic: true, format: "png", size: 4096 }))
+                .addField(
+                    "Currently Supported Docs",
+                    ["discord.js", "Javascript (mdn)"].map((str) => `\`${str}\``).join(", "),
+                )
+                .addField("Dependencies", Formatters.codeBlock("json", JSON.stringify(dependencies, undefined, 4)))
+                .addField("Uptime", `${intervalObjToStr(uptime)}` || "Just turned on")
+                .addField("Ping", message.client.ws.ping + "ms", true)
+                .addField("Source", "[Github](https://github.com/the-programmers-hangout/tph-docs-bot/)", true)
+                .addField(
+                    "Contributors",
+                    "[Link](https://github.com/the-programmers-hangout/tph-docs-bot/graphs/contributors)",
+                    true,
+                );
 
-        const statusEmbed = new MessageEmbed()
-            .setTitle(`${clientUser.tag} ${version}`)
-            .setURL("https://github.com/the-programmers-hangout/tph-docs-bot/")
-            .setColor(0x90ee90)
-            .setDescription(description)
-            .addField(
-                "Currently Supported Docs",
-                ["discord.js", "Javascript (mdn)"].map((str) => `\`${str}\``).join(", "),
-            )
-            .addField("Dependencies", Formatters.codeBlock("json", JSON.stringify(dependencies, undefined, 4)))
-            .addField(
-                "Uptime",
-                `${uptime.months} months, ${uptime.days} days, ${uptime.hours} hours, ${uptime.minutes} minutes, ${uptime.seconds} seconds`,
-            )
-            .addField("Ping", message.client.ws.ping + "ms", true)
-            .addField("Source", "[github](https://github.com/the-programmers-hangout/tph-docs-bot/)", true)
-            .addField(
-                "Contributors",
-                "[link](https://github.com/the-programmers-hangout/tph-docs-bot/graphs/contributors)",
-                true,
-            );
-
-        message.reply({ embeds: [statusEmbed] }).catch(console.error);
+            await message.reply({ embeds: [statusEmbed] });
+        }
+    } catch (e) {
+        console.error(e);
     }
+
     return;
 }
